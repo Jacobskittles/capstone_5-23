dc07645d4df04d9af2d7a46ef1451b07a1d7d773
cricklette <ellahlincoln@gmail.com>
2023-05-24 13:34:02 -0700

    added git ignore

--- expressapp.js
+++ expressapp.js
@@ -1,6 +1,6 @@
 // Lincoln. initialize express app. nodemon expressapp.js to monitor
 
-//How to connect MongoDB: 
+//How to connect MongoDB using the web interface: 
 // 1. Ensure that you have MongoDB compass installed and mongo initialized in the terminal.
 // 2. Open Mongo Atlas online, create your project, cluster, database. 
 // 3. Press "Connect" next to your cluster name in the UI. Go to drivers. follow the instructions ensuring you have proper credentials, you may need to reset your password.
@@ -22,7 +22,7 @@ app.listen(port, () => {
 //Lincoln. This is some code I assembled to connect the database to the javascript.
 const { MongoClient, ServerApiVersion } = require('mongodb');
 // find the uri by connecting your cluster to your node.js. replace with the link found in the instructions on the MongoDB cloud website. Must have proper credentials assigned to the cluster.
-const uri = "mongodb+srv://<username>:<password>@cluster0.jdcsztd.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongodb+srv://ellahlincoln:examplepass@excluster.d8hkgut.mongodb.net/?retryWrites=true&w=majority";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
