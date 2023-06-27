9b66fe712f0b05bac352ae18aebe4b699129e439
Jacob <Jacobskittles000@gmail.com>
2023-06-27 13:01:56 -0700

    created constructor for people objects in ejs

--- expressapp.js
+++ expressapp.js
@@ -1,17 +1,6 @@
 //  6/2/2023 run using command `npm run dev` can be found in package.json under scripts
 //  Lincoln. initialize express app. nodemon expressapp.js to monitor
 
-//EJS - npm install ejs
-
-//How to connect MongoDB using the web interface: 
-// 1. Ensure that you have MongoDB compass installed and mongo initialized in the terminal.
-// 2. Open Mongo Atlas online, create your project, cluster, database. 
-// 3. Press "Connect" next to your cluster name in the UI. Go to drivers. follow the instructions ensuring you have proper credentials, you may need to reset your password.
-// Open MongoDB compass. New Connection. Advanced. Mongodb+srv. Input the same string from before with credentials. 
-// Use the code below to connect via express app. You should now be connected to the database in the compass as well as in the app.
-
-//How to connect to MongoDB via command line? 
-
 //  Lincoln's code
 const express = require('express')
 const app = express()
@@ -46,6 +35,51 @@ function handleErrors(err, req, res, next){
   res.status(err.httpStatusCode || 500).send("Oh no, an error occurred!")
 }
 
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`)
+})
+
+
+/*
+----------------------------------------
+|            Mongo Stuff               |
+----------------------------------------
+*/
+
+
+//  Lincoln - This is some code I assembled to connect a web-hosted database to the javascript. 
+const { MongoClient } = require('mongodb');
+const { error } = require('console');
+
+const uri = "mongodb://10.10.20.152:27017";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri);
+
+//  This is a try/catch to ensure that the database connection is successful. 
+async function dbconnect() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    console.log('You have successfully connected to MongDB');
+  } catch (error) {
+    console.error("Error connecting to MongoDB", error)
+  }
+}
+dbconnect().catch(console.dir);
+
+const db = client.db("CBProjects")
+
+let personnel; 
+let projects;
+
+async function filldata() {
+  personnel = await db.collection("personnel").find().toArray();
+  projects = await db.collection("projects").find().toArray();
+}
+
+filldata()
+
 //  Going to localhost itself will simply redirect to the login screen 
 app.get('/', (req, res) => {
   res.redirect('/login')
@@ -57,10 +91,10 @@ app.get('/login', (req, res) => {
 })
 
 //  Going to the projects page. Lincoln + Gonzales
-app.get('/projects', (req, res) => {
+app.get('/projects', async (req, res) => {
     //  This code checks to see if credentials are successful and stored as a cookie
   if(req.cookies.login == 'true'){
-    res.render('pages/index')
+    res.render('pages/index', {personnel: personnel, projects: projects})
   }else{
     //  Redirects to login page if the credentials are not successfully stored
     res.redirect('/login')
@@ -114,39 +148,6 @@ app.get('/logout', (req, res) => {
 
 
 
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`)
-})
-
-//  Lincoln - This is some code I assembled to connect a web-hosted database to the javascript. 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-//  Find the uri by connecting your cluster to your node.js. replace with the link found in the instructions on the MongoDB cloud website. Must have proper credentials assigned to the cluster.
-//  You can replace with local database host
-const uri = "mongodb+srv://ellahlincoln:examplepass@excluster.d8hkgut.mongodb.net/?retryWrites=true&w=majority";
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
-
-//  This is a try/catch to ensure that the database connection is successful. 
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
 
 
 
