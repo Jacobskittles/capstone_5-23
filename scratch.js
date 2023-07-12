ea42bf12f84770e98f67ee03b829b1225342ba9b
Digit <digit.10k@gmail.com>
2023-07-12 14:32:35 -0700

    test 2

--- scratch.js
+++ scratch.js
@@ -1,6 +1,6 @@
 const { MongoClient, ObjectId } = require('mongodb');
 
-const uri = "mongodb://localhost:27017";
+const uri = "mongodb://127.0.0.1:27017";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri);
@@ -24,19 +24,24 @@ const projects = db.collection("projects")
 
 async function join(projectID, personID) {
   let projectQuery = { _id: projectID };
-  let personQuery = { _id: personID };
-  
+  let personQuery = { _id: personID};
+
+  let person, project
   try {
-    const person = await personnel.findOne(personQuery);
-    if (person) {
-      console.log(person);
-    } else {
+    person = await personnel.findOne(personQuery);
+    project = await projects.findOne(projectQuery);
+    if (!person || !project) {
       console.log("no result found");
+      return;
     }
   } catch (error) {
     console.log("ERROR: " + error);
   }
+
+  if (person.projecr)
+
+  console.log(person, project)
 }
 
 
-join("d506edfb-737a-4681-be14-688d82770d76", "4c5a68aa-0725-4f18-b74b-89bb75c85f05")
+join("4c5a68aa-0725-4f18-b74b-89bb75c85f05", "0e9d1d41-7517-4916-aa5e-b654fb550169")
