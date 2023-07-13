eaed324320426b62ad0b55779154510831763887
Digit404 <digit.10k@gmail.com>
2023-07-13 10:48:56 -0700

    more functions

--- scratch.js
+++ scratch.js
@@ -1,4 +1,5 @@
 const { MongoClient, ObjectId } = require("mongodb");
+const crypto = require("crypto");
 
 const uri = "mongodb://127.0.0.1:27017";
 
@@ -17,12 +18,18 @@ async function dbconnect() {
 }
 dbconnect().catch(console.dir);
 
-const db = client.db("CBTest");
+const db = client.db("CBProjects");
 
 const personnel = db.collection("personnel");
 const projects = db.collection("projects");
 
 async function unjoin(projectID, personID) {
+    /**
+     * "Unjoins" a person and project
+     * @param {string} projectID - The ID of the project to join.
+     * @param {string} personID - The ID of the person to join.
+     * @returns {Promise<void>} - A promise that resolves once the operation is complete.
+     */
     let projectQuery = { _id: projectID };
     let personQuery = { _id: personID };
 
@@ -42,18 +49,43 @@ async function unjoin(projectID, personID) {
     let assignments = person.projects;
     let members = project.members;
 
-    let assignment = person.projects.find(
-        (assignment) => assignment.id === projectID
-    );
-    let member = project.members.find((member) => member.id === personID);
+    if (!assignments || !members) return; // this person has no projects
 
-    console.log(assignments, members);
+    // this isn't joined
+    if (
+        !assignments.find((assignment) => assignment.id === projectID) ||
+        !members.find((member) => member.id === personID)
+    )
+        return;
+
+    assignments = assignments.filter(
+        (assignment) => assignment.id !== projectID
+    );
+    members = members.filter((member) => member.id !== personID);
+
+    // push to database
+    try {
+        await personnel.updateOne(personQuery, {
+            $set: { projects: assignments },
+        });
+        await projects.updateOne(projectQuery, {
+            $set: { members: members },
+        });
+    } catch (error) {
+        console.log("ERROR: " + error);
+    }
 }
 
 async function join(projectID, personID) {
+    /**
+     * Joins a person to a project in the database.
+     * @param {string} projectID - The ID of the project to join.
+     * @param {string} personID - The ID of the person to join.
+     * @returns {Promise<void>} - A promise that resolves once the join operation is complete.
+     */
     // set up the queries
-    let projectQuery = { _id: projectID };
-    let personQuery = { _id: personID };
+    const projectQuery = { _id: projectID };
+    const personQuery = { _id: personID };
 
     //create and load the person and project objects with queries
     let person, project;
@@ -74,22 +106,22 @@ async function join(projectID, personID) {
     if (!project.members) project.members = [];
 
     // check if project and member are already joined
-    let assignment = person.projects.find(
+    const assignment = person.projects.find(
         (assignment) => assignment.id === projectID
     );
-    let member = project.members.find((member) => member.id === personID);
+    const member = project.members.find((member) => member.id === personID);
 
     // if they aren't the same (member and project were improperly joined somehow)
     if ((assignment !== undefined) ^ (member !== undefined)) {
         console.log("Project and member improperly joined (?). Fixing...");
         if (!assignment) {
-            let newAssignment = { id: projectID };
+            const newAssignment = { id: projectID };
             if (member.role)
                 // if, for some bizarre reason, the improper join has a role assigned
                 newAssignment.role = member.role;
             person.projects.push(newAssignment);
         } else {
-            let newMember = { id: personID };
+            const newMember = { id: personID };
             if (assignment.role) newMember.role = assignment.role;
             project.members.push(newMember);
         }
@@ -103,14 +135,169 @@ async function join(projectID, personID) {
 
     // finally, push to the database, updating only the projects and members arrays
     try {
-        await personnel.updateOne(personQuery, { $set: {projects: person.projects }});
-        await projects.updateOne(projectQuery, { $set: {members: project.members }});
+        await personnel.updateOne(personQuery, {
+            $set: { projects: person.projects },
+        });
+        await projects.updateOne(projectQuery, {
+            $set: { members: project.members },
+        });
     } catch (error) {
         console.log("ERROR: " + error);
     }
 }
 
-unjoin(
-    "4c5a68aa-0725-4f18-b74b-89bb75c85f05",
-    "0e9d1d41-7517-4916-aa5e-b654fb550169"
-);
+async function changeRole(projectID, personID, role) {
+    const projectQuery = { _id: projectID };
+    const personQuery = { _id: personID };
+
+    //create and load the person and project objects with queries
+    let person, project;
+    try {
+        person = await personnel.findOne(personQuery);
+        project = await projects.findOne(projectQuery);
+        if (!person || !project) {
+            console.log("Result not found");
+            return;
+        }
+    } catch (error) {
+        console.log("ERROR: " + error);
+    }
+
+    const assignments = person.projects;
+    const members = project.members;
+
+    // THERE CAN ONLY BE ONE!
+    if (role === "Lead") {
+        for (let member of members) {
+            if (member.role === "Lead") delete member.role;
+        }
+    }
+
+    // Find index of assignment and member
+    const assignmentIndex = assignments.findIndex(
+        (assignment) => assignment.id === projectID
+    );
+    const memberIndex = members.findIndex((member) => member.id === personID);
+
+    // Check if assignment and member were found
+    if (assignmentIndex === -1 || memberIndex === -1) {
+        return;
+    }
+
+    // Update the role
+    assignments[assignmentIndex].role = role;
+    members[memberIndex].role = role;
+
+    // push to database
+    try {
+        await personnel.updateOne(personQuery, {
+            $set: { projects: assignments },
+        });
+        await projects.updateOne(projectQuery, {
+            $set: { members: members },
+        });
+    } catch (error) {
+        console.log("ERROR: " + error);
+    }
+}
+
+async function createPerson(person) {
+    //generate new ID and insert into db
+    person._id = crypto.randomUUID();
+    personnel.insertOne(person, (err, result) => {
+        if (err) {
+            console.error("Failed to insert person:", err);
+        }
+    });
+    return person._id
+}
+
+async function createProject(project) {
+    //generate new ID and insert into db
+    project._id = crypto.randomUUID();
+    projects.insertOne(project, (err, result) => {
+        if (err) {
+            console.error("Failed to insert person:", err);
+        }
+    });
+    return project._id
+}
+
+async function deletePerson(personID) {
+    const personQuery = { _id: personID };
+
+    let person;
+    try {
+        person = await personnel.findOne(personQuery);
+        if (!person) {
+            console.log("Result not found");
+            return;
+        }
+    } catch (error) {
+        console.log("ERROR: " + error);
+    }
+
+    for (assignment of person.projects) {
+        unjoin(assignment.id, personID);
+    }
+
+    personnel.deleteOne(personQuery, (err, result) => {
+        if (err) {
+            console.error("Failed to delete person: ", err);
+        }
+    });
+}
+
+async function deleteProject(projectID) {
+    const projectQuery = { _id: projectID };
+    let project;
+    try {
+        project = await projects.findOne(projectQuery);
+        if (!project) {
+            console.log("Result not found");
+            return;
+        }
+    } catch (error) {
+        console.log("ERROR: " + error);
+    }
+
+    for (member of project.members) {
+        unjoin(projectID, member.id);
+    }
+
+    projects.deleteOne(projectQuery, (err, result) => {
+        if (err) {
+            console.error("Failed to delete project: ", err);
+        }
+    });
+}
+
+async function doThing() {
+    const ianID = await createPerson({ firstName: "Ian", lastName: "Gronemier" });
+
+    const projectID = await createProject({
+        name: "Kick bricks",
+        description:
+            "If you got this assignment that means no one wants you at the squadron",
+    });
+
+    console.log(ianID, projectID)
+
+    await join(projectID, ianID);
+
+    await changeRole(projectID, ianID, "Lead");
+
+    await unjoin(projectID, ianID)
+
+    await deletePerson(ianID);
+
+    await deleteProject(projectID);
+}
+
+async function test() {
+    let dowuh = await createPerson({firstName: "ernest", lastName: "carrasco"})
+    console.log(dowuh)
+}
+doThing()
+
+//test()
